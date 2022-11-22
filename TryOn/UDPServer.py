17f664102113846623586b9cb95fc96808cbb65f
Marion SALAUN <marion.salauen.001@student.uni.lu>
2022-11-22 19:24:24 +0100

    Network1FirstUDPServerClientMadeAndWork

--- TryOn/UDPServer.py
+++ TryOn/UDPServer.py
@@ -1,4 +1,21 @@
 import socket
+
+#TUTO follow from Programming Knowledge (cf sources in README)
+
 sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 #'AF_INET'belongs to the IPV4 family
+#UDP (SOCK_DGRAM) is a datagram-based protocol. You send one datagram and get one reply and then the connection terminates
+
 sock.bind(('127.0.0.1',12345))
+# '127.0.0.1': IP address
+# 12345 : port number 
+
+while True:
+    data,addr=sock.recvfrom(4096)
+    print(data)
+    # UDP adress will change at the opposite of TCP
+    # '4096' : number of bytes provided
+
+    message = bytes(("Hello I am UDP server").encode('utf-8'))
+    #convert 'message' in bytes
+    sock.sendto(message, addr)
---
+++ TryOn/readme.txt
@@ -0,0 +1,8 @@
+TO RUN the program :
+
+1. First you need to run 'UDPServer.py' in a terminal : $ python UDPServer.py
+2. Then you need to run 'UDPClient.py' into a different terminal window : $ python UDPClient.py
+3. Normally you should have :
+
+In UDPServer terminal you should see :  ` b'Hello UDP server ! I am the Client !' `
+In UDPClient terminal you should see :  ` Server says b'Hello I am UDP server' `
