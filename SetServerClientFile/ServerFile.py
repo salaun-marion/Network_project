da6cde93e96d9e281cd277554d7c4aada0b6c194
Marion SALAUN <marion.salauen.001@student.uni.lu>
2022-11-26 21:34:13 +0100

    FailForSendingFile

--- SetServerClientFile/ServerFile.py
+++ SetServerClientFile/ServerFile.py
@@ -1,14 +1,16 @@
+#NOT WORKING AT ALL ^^
+
 import socket 
 
 sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-sock.bind('127.0.0.1',12345)
+sock.bind(('127.0.0.1',12345))
 
 while True :
     data,addr=sock.recvfrom(4096)
     print(data)
-    
-    src='SetServerClientFile/hello.txt'
-    file=bytes(src.encode('utf-8'))
 
-    sock.sendto()
+    src = open("/Users/marion/Documents/00_BINFO2/3.5.Networks_1/Network_project/SetServerClientFile/hello.txt", "rb")
+    file=bytes(src)
+
+    sock.sendto(src,addr)
